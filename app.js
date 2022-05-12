8cdf1ebb9de33219c3bf666354a81224fcd84b47
AffouSiata <affousiatao59@gmail.com>
2022-05-12 18:38:06 +0000

    continuer-front

---
+++ app.js
@@ -0,0 +1,26 @@
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+
+
+  $('.carousel').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  
+
+
+
+
+
+  
+
+
+ 
