615c3538d51fc62a78ff0b4b99a624119fa50a22
AffouSiata <affousiatao59@gmail.com>
2022-05-13 17:58:23 +0000

    encore

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-$('.slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
+// $('.partie2').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     autoplay: true,
+//     cssEase: 'linear'
+//   });
 
 
-  $('.carousel').slick({
-    slidesToShow: 4,
+
+
+
+  $('.partie2').slick({
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
   });
+
+
+  // $('.carousel').slick({
+  //   slidesToShow: 4,
+  //   slidesToScroll: 1,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000,
+  // });
 
   
 
