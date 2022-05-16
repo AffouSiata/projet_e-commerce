e385a08d74732682744ff0619cf92453245a26b8
AffouSiata <affousiatao59@gmail.com>
2022-05-16 18:35:42 +0000

    ajout des diiferentes pages

---
+++ mm.js
@@ -0,0 +1,64 @@
+$('.partie2').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+let menu = document.querySelector('#menu')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+   navbar.classList.toggle('active')
+
+}
+window.onscroll = ()=>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".product-slider", {
+  loop:true,
+  spaceBetween: 20,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+
+
+var swiper = new Swiper(".review-slider", {
+  loop:true,
+  spaceBetween: 20,
+  autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
