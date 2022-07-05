4edc29fc976abbfe020a48fd08a9bc88b7c04a70
AffouSiata <affousiatao59@gmail.com>
2022-07-05 14:58:49 +0000

    dcxfdf

--- database/bd.js
+++ database/bd.js
@@ -1,11 +1,22 @@
-let mysql = require('mysql');
-let connect = mysql.createConnection({
+// let mysql = require('mysql');
+// let connect = mysql.createConnection({
    
-   host:'192.168.64.2',
-   user: 'Affou',
-   password:'12345',
-   database:'projet_e-commerce'
+//    host:'192.168.64.2',
+//    user: 'Affou',
+//    password:'12345',
+//    database:'projet_e-commerce'
 
+// });
+
+// module.exports=connect;
+
+
+const { Client } = require('pg');
+
+const connect = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
-module.exports=connect;
+module.exports=connect
